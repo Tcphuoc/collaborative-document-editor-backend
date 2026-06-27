b37bb64ede6e4e1ec2d0ded7ae86cd581349600a
Tcphuoc <tcphuoc0511@gmail.com>
2026-06-27 12:57:30 +0000

    T1: Create document migration

--- database/migrations/2026_06_27_115147_create_documents_table.php
+++ database/migrations/2026_06_27_115147_create_documents_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('password');
+        Schema::create('documents', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->string("title");
+            $table->text("content");
+            $table->foreignId('created_user_id')->constrained("users", "id")->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('documents');
     }
 };
